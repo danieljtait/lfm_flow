e7e123d2277bbbb5dd41e5944c1c72ac35b2e574
danieljtait <tait.djk@gmail.com>
2019-03-20 22:43:54 +0000

    lfm kernel rewritten for batching and to behave life tfp Kernel

--- lfm_flow/kernels/lfm_kernels.py
+++ lfm_flow/kernels/lfm_kernels.py
@@ -3,6 +3,15 @@ import numpy as np
 from .kernels import Kernel
 
 
+def _validate_lfmkern_input(t1, output_ndims):
+    if not isinstance(t1, (list, tuple)):
+        raise ValueError("Input must be a list-like of length {}".format(output_ndims))
+
+    else:
+        shape = [item.shape[-1] for item in t1]
+        t1 = np.concatenate([item for item in t1])
+        return t1, shape
+
 class LFM1_RBF(Kernel):
     """
     First order LFM kernel
@@ -14,6 +23,7 @@ class LFM1_RBF(Kernel):
         self._D = tf.get_variable('D',
                                   dtype=dtype,
                                   initializer=np.asarray(D))
+        self._output_ndims = self._D.shape[0]
 
         self._S = tf.get_variable('S',
                                   dtype=dtype,
@@ -29,81 +39,112 @@ class LFM1_RBF(Kernel):
 
     @property
     def lf_length_scales(self):
+        """ Length scales of the latent force RBF kernels. """
         return self._lf_length_scales
     #ls = tf.exp(self._sp_length_scales) - 1
     #    return tf.log(ls) - (np.finfo(np.float64).tiny)
 
     @property
     def D(self):
+        """ First order ODE coefficients. """
         return self._D
 
     @property
     def S(self):
-        return self._S
+        """ Sensitivity matrix. """
+        return self._S    
+
+    @property
+    def output_ndims(self):
+        return self._output_ndims
+
+    def _batch_shape(self):
+        # calculates the batch shape from the parameter broadcast shapes
+        batch_shape = tf.broadcast_static_shape(self.D[..., None].shape,
+                                                self.S.shape)
+        batch_shape = tf.broadcast_static_shape(
+            batch_shape,
+            self.lf_length_scales[...., None, :].shape)
+        return batch_shape[:-2]
+
+    def _batch_shape_tensor(self):
+
+        batch_shape = tf.broadcast_dynamic_shape(
+            tf.shape(self.D[..., None]), tf.shape(S))
+
+        return batch_shape[:-2]
 
     def _hpq(self, t1, t2, shape1, shape2):
         D = self.D
         lf_length_scales = self.lf_length_scales
-        R = self.lf_length_scales.shape[0]
+        R = self.lf_length_scales.shape[-1]
 
-        Dt = (t1[:, None] - t2[None, :])[..., None] / lf_length_scales
+        Dt = x1[..., None, :] - x2[..., None, :, :]
+        # scale Dt by length_scales
+        # -- pad length scales
+        # Dt.shape = broadcast([b1,...,bB], [c1,...,cC]) + [e1, e2, R]
+        Dt /= self.lf_length_scales[..., None, None, :]
 
-        #nup[p, r] = .5 * D[p] * l[r]
-        nup = .5 * D[:, None] * lf_length_scales[None, :]
-        #inflate nup
-        nup = tf.concat([nup[p, :]*tf.ones((Np, R), dtype=D.dtype)
+        #nup[..., p, r] = .5 * D[..., p] * l[..., r]
+        nup = .5 * D[..., :, None] * lf_length_scales[..., None, :]
+
+        # inflate nup to shape [b1,...,bB, e1, R]
+        nup = tf.concat([nup[..., p, None, :] * tf.ones((Np, R), dtype=D.dtype)
                          for p, Np in enumerate(shape1)],
-                        axis=0)
+                        axis=-2)
 
-        expr1 = tf.erf(Dt - nup[:, None, :]) + \
-                tf.erf(t2[None, :, None] / lf_length_scales
-                    + nup[:, None, :])
-        Dp_shape1 = tf.concat([D[p] * tf.ones(Np, dtype=D.dtype)
+        expr1 = tf.erf(Dt - nup[..., :, None, :]) + \
+                tf.erf((x2 / lf_length_scales[..., None, :])[..., None, :, :] +
+                       nup[..., None, :])
+
+        # Pad Dp 
+        Dp_shape1 = tf.concat([D[..., p, None, None] * tf.ones((Np, 1), dtype=D.dtype)
                                for p, Np in enumerate(shape1)],
-                              axis=0)
+                              axis=-2)
+        expr1 *= tf.exp(Dp_shape1[..., None, :] * x2[..., None, :, :])
 
-        expr1 *= tf.exp(Dp_shape1[:, None] * t2[None, :])[..., None]
-
-        # pad D to conform with shape of second arg
-        Dq_shape2 = tf.concat([D[q] * tf.ones(Nq, dtype=D.dtype)
+        Dq_shape2 = tf.concat([D[..., q, None, None] * tf.ones((Nq, 1), dtype=D.dtype)
                                for q, Nq in enumerate(shape2)],
-                              axis=0)
-        expr2 = tf.erf(t1[:, None, None] / lf_length_scales - nup[:, None, :]) + \
-                tf.erf(nup[:, None, :])
+                              axis=-2)
 
-        expr2 = expr2 * tf.exp(-Dq_shape2 * t2)[None, :, None]
+        expr2 = tf.erf(x1 / lf_length_scales[..., None, :] - nup) + tf.erf(nup)
+        expr2 = expr2[..., None, :] * tf.exp(-Dq_shape2 * x2)[..., None, :, :]
 
-        C = tf.exp(-Dp_shape1 * t1)[:, None] / (Dp_shape1[:, None] +
-                                                Dq_shape2[None, :])
-        C = C[..., None] * tf.exp(nup ** 2)[:, None, :]
+        C = tf.exp(-Dp_shape1 * x1)[..., None, :] / \
+            (Dp_shape1[..., None, :] + Dq_shape2[..., None, :, :])
 
         return C * (expr1 - expr2)
 
-    def __call__(self, t1, shape1, t2=None, shape2=None):
-        if t2 is None:
-            t2 = tf.identity(t1)
-            shape2 = shape1.copy()
+    def _apply(self, x1, x2=None, x1_shape=None, x2_shape=None):
+        if x2 is None:
+            x2 = tf.identity(x1)
+            x2_shape = x1_shape.copy()
 
-        R = self.lf_length_scales.shape[0]
+        R = self.lf_length_scales.shape[-1]
         
-        # inflate to S[r, p] * ones(Np)
-        Srp = tf.concat([self.S[p, :][None, :] * tf.ones((Np, R),
-                                                         dtype=self.D.dtype)
-                         for p, Np in enumerate(shape1)],
-                        axis=0)
-        Srq = tf.concat([self.S[q, :][None, :] * tf.ones((Nq, R),
-                                                         dtype=self.D.dtype)
-                         for q, Nq in enumerate(shape1)],
-                        axis=0)
-        C = Srp[:, None, :] * Srq[None, ...]
-        C *= .5 * np.sqrt(np.pi) * self.lf_length_scales
+        Srp = tf.concat([self.S[..., p, :][..., None, :] * \
+                         tf.ones((Np, R), dtype=self.D.dtype)
+                         for p, Np in enumerate(x1_shape)],
+                        axis=-2)
+        Srq = tf.concat([self.S[..., q, :][..., None, :] * \
+                         tf.ones((Nq, R), dtype=self.D.dtype)
+                         for q, Nq in enumerate(x2_shape)],
+                        axis=-2)
 
-        hpq_t1t2 = self._hpq(t1, t2, shape1, shape2)
-        hqp_t2t1 = self._hpq(t2, t1, shape2, shape1)
-        hqp_t2t1 = tf.transpose(hqp_t2t1, (1, 0, 2))
+        C = Srp[..., None, :] * Srq[..., None, :, :]
+        C *= .5 * np.sqrt(np.pi) * self.lf_length_scales[..., None, None, :]
+
+        hpq_t1t2 = self._hpq(x1, x2, x1_shape, x2_shape)
+        hqp_t2t1 = self._hpq(x2, x1, x2_shape, x1_shape)
+
+        perm = [*range(len(hqp_t2t1.shape))]
+        perm[-3], perm[-2] = perm[-2], perm[-3]
+        
+        hqp_t2t1 = tf.transpose(hqp_t2t1, perm)
 
         cov = (hpq_t1t2 + hqp_t2t1) * C
         cov = tf.reduce_sum(cov, axis=-1)
+
         return cov
 
     def lf_cross_cov(self, t1, shape, t2):
@@ -141,7 +182,7 @@ class LFM1_RBF(Kernel):
 
         Dt = t1[:, None] - _t2[None, :]
         C = .5 * np.sqrt(np.pi) * _S * lr[None, :] * tf.exp(nu**2)
-
+        
         expr1 = tf.exp(-D[:, None] * Dt)
 
         expr2 = tf.erf( Dt / lr[None, :] - nu) + \
--- lfm_flow/latentforcemodels/llfm.py
+++ lfm_flow/latentforcemodels/llfm.py
@@ -11,7 +11,7 @@ class LLFM:
         """
         Tensorflow function to compute the multivate normal log likelihood
         """
-        Kyy = self.kernel(self.t_input, self.t_input_shape)
+        Kyy = self.kernel(self.t_input)#, self.t_input_shape)
         Kyy += tf.diag(1e-4 * tf.ones(Kyy.shape[0], dtype=Kyy.dtype))
         mvn = MultivariateNormalFullCovariance(covariance_matrix=Kyy)
 
@@ -19,27 +19,48 @@ class LLFM:
             tf.transpose(self.Y, (1, 0)))
         return tf.reduce_sum(logpdf)
 
+    def predict(self, tnew):
+        Kyy = self.kernel(self.t_input)
+        Kyy += tf.diag(self.jitter * tf.ones(Kyy.shape[0], dtype=Kyy.dtype))
+        Lyy = tf.linalg.cholesky(Kyy)
+
+        Kyy_ = self.kernel(self.t_input, tnew)
+        Ky_y = tf.transpose(Kyy_, (1, 0))
+
+        Ky_y_ = self.kernel(tnew)
+        Kcond = Ky_y_ - \
+                tf.matmul(Ky_y, tf.linalg.cholesky_solve(Lyy, Kyy_))
+
+        return tf.matmul(Ky_y,
+                         tf.linalg.cholesky_solve(Lyy, self.Y)), Kcond
+
+
     def predict_lf(self, tnew, return_cov=False):
         """
         Predicts the value of the latent forces at times tnew
         """
 
-        Kyy = self.kernel(self.t_input, self.t_input_shape)
+        Kyy = self.kernel(self.t_input)
         Kyy += tf.diag(self.jitter * tf.ones(Kyy.shape[0], dtype=Kyy.dtype))
 
         # Cholesky decomposition
         Lyy = tf.linalg.cholesky(Kyy)
 
-        Kyf = self.kernel.lf_cross_cov(self.t_input,
-                                       self.t_input_shape,
+        import numpy as np
+
+        Kyf = self.kernel.lf_cross_cov(np.concatenate(self.t_input),
+                                       [item.size for item in self.t_input],
                                        tnew)
+
         mf = tf.matmul(
             tf.transpose(Kyf, (1, 0)),
             tf.linalg.cholesky_solve(Lyy, self.Y))
 
         if return_cov:
+            tnew = np.concatenate([tnew, tnew])
 
             Dtsq = tnew[:, None] - tnew[None, :]
+
             Dtsq = Dtsq[None, ...] / self.kernel.lf_length_scales[:, None, None]
             Dtsq = Dtsq ** 2
 
